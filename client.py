3e1184de7c47517770342930bd7e147fff28e660
zalkar-z <zak@packet.com>
2020-03-31 11:49:49 -0400

    updated ip addresses

--- client.py
+++ client.py
@@ -9,8 +9,8 @@
 import socket
 import sys
 
-# UDP_ADDRESS = '10.10.117.73'
-UDP_ADDRESS = '127.0.0.1'
+UDP_ADDRESS = '10.10.117.79'
+# UDP_ADDRESS = '127.0.0.1'
 UDP_PORT = 9000
 MESSAGE = sys.argv[1]
 # MESSAGE = "hello"
@@ -19,13 +19,14 @@ MESSAGE = sys.argv[1]
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # set timeout
-sock.settimeout(5)
+# sock.settimeout(5)
 
-# send out message to the server - no connect/close needed!
-sock.sendto(MESSAGE.encode(), (UDP_ADDRESS, UDP_PORT))
-print("Sent message: '{0}' to {1}".format(MESSAGE, UDP_ADDRESS))
+while True:
+    # send out message to the server - no connect/close needed!
+    sock.sendto(MESSAGE.encode(), (UDP_ADDRESS, UDP_PORT))
+    print("Sent message: '{0}' to {1}".format(MESSAGE, UDP_ADDRESS))
 
-# receiving some updated data
-data, addr = sock.recvfrom(1024) # 1024-byte buffer size
-data = data.decode()
-print("{0} received: {1}".format(addr[0], data))
+    # receiving some updated data
+    # data, addr = sock.recvfrom(1024) # 1024-byte buffer size
+    # data = data.decode()
+    # print("{0} received: {1}".format(addr[0], data))
