8a6f1de7916a9f8a9a0f7273c1a4722def01818f
zalkar-z <zak@packet.com>
2020-03-30 21:22:23 -0400

    initial client and server setup

---
+++ server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+	server.py - UDP server that listens on UDP port 9000 and prints what was sent to it.
+	Author: Zak Ziiaidin (zalkar@bennington.edu)
+	Date: 3/3/2020
+"""
+
+import socket
+
+# UDP_ADDRESS = '10.10.117.73'
+UDP_ADDRESS = '127.0.0.1'
+UDP_PORT = 9000
+# MESSAGE = sys.argv[1]
+
+# create a socket -DGRAM == UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# bind our socker to the given address/port
+sock.bind((UDP_ADDRESS, UDP_PORT))
+
+# my fancy server logic
+while 1:
+    data, addr = sock.recvfrom(1024) # 1024-byte buffer size
+    data = data.decode()
+    print("{0} received: {1}".format(addr[0], data))
+
+    data = data + '!'
+
+    # sending updated data back
+    sock.sendto(data.encode(), addr)
